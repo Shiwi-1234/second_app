6537afaa2e6d7de35517aa558ef2a9763ec1e4af
Shiwi-1234 <shiwani@preciousinfosystem.com>
2023-01-18 19:28:50 +0530

    new change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,30 @@
 class ArticlesController < ApplicationController
 require 'httparty'#require 'rest-client'
 
-
-def edit 
-  @article = Article.find(params[:id])
-end
-  
 def index
-   url = "http://localhost:3001/todolists"
-   response = HTTParty.get(url)
+  url = "http://localhost:3001/articles"
+  response = HTTParty.get(url)
   @data = JSON.parse(response.body)
- # render json: @data
-
- end
-
-
-
-
-def update
-  @article = Article.find(params[:id])
-  if @article.update(articles_params)
-    redirect_to articles_path
-  else
-    render 'edit'
-  end
+  
 end
+
+def show
+   url = "http://localhost:3001/articles/6"
+  response = HTTParty.get(url)
+  @data = JSON.parse(response.body)
+end
+
+def update_item
+  response = HTTParty.put("http://localhost:3001/articles/5", body: {name: "updated item name"})
+  puts response.body
+end
+
+
 
 private
-
 def articles_params
    params.require(:article).permit(:title, :description, :date, :day )
 end
-
-
-def gets_articles
-  url = "http://localhost:3001/todolists"
-  response = HTTParty.get(url)
-  data = JSON.parse(response.body)
-  data.each do |item|
-    Article.create(title:item["name"])
-  end 
-  
-  render json: @data
-end
-
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/todo", to:"articles#gets_articles"
+ 
   resources :articles 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  get 'articles/create_item' 
 end
