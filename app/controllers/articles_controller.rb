e95c57a0a9d42abef9e6ad04387ebb0801d9f60b
Shiwi-1234 <shiwani@preciousinfosystem.com>
2023-01-19 19:36:54 +0530

    sending request on api

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,46 @@
 class ArticlesController < ApplicationController
 require 'httparty'#require 'rest-client'
+include HTTParty
+  base_uri "http://localhost:3001/"
+
 
 def index
   url = "http://localhost:3001/articles"
   response = HTTParty.get(url)
   @data = JSON.parse(response.body)
-  
 end
 
 def show
-   url = "http://localhost:3001/articles/6"
+  url = "http://localhost:3001/articles/6"
   response = HTTParty.get(url)
+  @data = JSON.parse(response.body)
+  
+end
+
+#def update_item
+  #options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, :debug_output => $stdout, body: { "param1" => 1, "param2" => 'sports'}
+  #.to_json, basic_auth: {:username=>'shiwi11@gmail.com', :password=>'123456'} }
+  #url = "http://localhost:3001/articles/6"
+  #response = HTTParty.patch(url, options)
+ # @data = response.parsed_response
+#end
+
+#def create_item
+ # HTTParty.patch('http://localhost:3001/articles/6', body: { id: 1, title: "sports" })
+#end
+
+def update_item
+  options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, 
+  body: { "id" => 1, "title" => 'office', "description" => 'The second home', "date" => "19/01/2023", "Day" => "Thursday"}
+  .to_json}
+
+  url = "http://localhost:3001/articles/10"
+  response = HTTParty.patch(url, options)
   @data = JSON.parse(response.body)
 end
 
-def update_item
-  response = HTTParty.put("http://localhost:3001/articles/5", body: {name: "updated item name"})
-  puts response.body
+
+
+def display
 end
-
-
-
-private
-def articles_params
-   params.require(:article).permit(:title, :description, :date, :day )
-end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
- 
-  resources :articles 
-  get 'articles/create_item' 
+   resources :articles do
+    collection do
+      get :update_article
+      get :display
+     end
+    end  
+  get 'articles/show_article' 
+  root 'articles#display'
 end
