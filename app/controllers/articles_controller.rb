894a27c2b1a4b395c2e40c78ab462031d0af373a
Shiwi-1234 <shiwani@preciousinfosystem.com>
2023-01-20 19:03:38 +0530

    Adding functionality to admin application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,34 +11,57 @@ def index
 end
 
 def show
-  url = "http://localhost:3001/articles/6"
+  url = "http://localhost:3001/articles/" + params[:id]
   response = HTTParty.get(url)
-  @data = JSON.parse(response.body)
   
 end
 
-#def update_item
-  #options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, :debug_output => $stdout, body: { "param1" => 1, "param2" => 'sports'}
-  #.to_json, basic_auth: {:username=>'shiwi11@gmail.com', :password=>'123456'} }
-  #url = "http://localhost:3001/articles/6"
-  #response = HTTParty.patch(url, options)
- # @data = response.parsed_response
-#end
+def edit
+ url = "http://localhost:3001/articles/" + params[:id]
+ response = HTTParty.get(url)
+ @data = JSON.parse(response.body)
+end
 
-#def create_item
- # HTTParty.patch('http://localhost:3001/articles/6', body: { id: 1, title: "sports" })
-#end
+def update_article
+  options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, body: { "published" => true}.to_json }
+ 
+  url = "http://localhost:3001/articles/10" 
+  response = HTTParty.patch(url , options)
+  status_code = response.code
+  @data = response.parsed_response
+  if status_code == 200
+    flash[:notice] = 'Your Article was Update Successfully!'
+  end 
+end
 
-def update_item
+def update
+  options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, body: { "published" => true}
+  .to_json }
+  #url = "http://localhost:3001/articles/" 
+  response = HTTParty.patch(@url , options)
+  status_code = response.code
+  @data = response.parsed_response
+  if status_code == 200
+    flash[:notice] = 'Your Article was Update Successfully!'
+  end 
+  redirect_to  update_article_articles_path
+end
+
+def create_item
   options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, 
   body: { "id" => 1, "title" => 'office', "description" => 'The second home', "date" => "19/01/2023", "Day" => "Thursday"}
   .to_json}
-
-  url = "http://localhost:3001/articles/10"
+  url = 'http://localhost:3001/articles/10'
   response = HTTParty.patch(url, options)
   @data = JSON.parse(response.body)
 end
 
+def published
+  url = "http://localhost:3001/articles"
+  response = HTTParty.get(url)
+  @data = JSON.parse(response.body)
+  @articles = @data.select{|article| article["published"] == true}
+end
 
 
 def display
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     collection do
       get :update_article
       get :display
+      get :published
      end
     end  
   get 'articles/show_article' 
