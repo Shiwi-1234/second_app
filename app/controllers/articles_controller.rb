fcada93e06bde5efe8db6a7d9dd8c3e4577e9ea4
Shiwi-1234 <shiwani@preciousinfosystem.com>
2023-01-24 19:15:17 +0530

    Add bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,6 @@ require 'httparty'#require 'rest-client'
 include HTTParty
   base_uri "http://localhost:3001/"
 
-
 def index
   url = "http://localhost:3001/articles"
   response = HTTParty.get(url)
@@ -24,10 +23,8 @@ def edit
 end
 
 def update
-  
-  
-  url = "http://api.example.com/articles/" + params[:id]
-  options = { headers: { "Accept" => "application/json", "Content-Type" => "application/json", }, body: data.to_json }
+  url = "http://localhost:3001/articles/"+ params[:id]
+  options = { headers: { "Accept" => "application/json", "Content-Type" => "application/json", }, body: {"title" => params['title'], "description" => params['description']}.to_json }
   response = HTTParty.patch(url, options)
   @data =  JSON.parse(response.body)
   if response.code == 200
@@ -40,34 +37,12 @@ def update
  end 
 end
 
-# def update_article
-#   options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, body: { "published" => true}.to_json }
- 
-#   url = "http://localhost:3001/articles/10" 
-#   response = HTTParty.patch(url , options)
-#   status_code = response.code
-#   @data = response.parsed_response
-#   if status_code == 200
-#     flash[:notice] = 'Your Article was Update Successfully!'
-#   end 
-# end
-
-def create_item
-  options = { headers: {"Accept"=>"application/json", "Content-Type"=>"application/json"}, 
-  body: { "id" => 1, "title" => 'office', "description" => 'The second home', "date" => "19/01/2023", "Day" => "Thursday"}
-  .to_json}
-  url = 'http://localhost:3001/articles/10'
-  response = HTTParty.patch(url, options)
-  @data = JSON.parse(response.body)
-end
-
 def published
   url = "http://localhost:3001/articles"
   response = HTTParty.get(url)
   @data = JSON.parse(response.body)
   @articles = @data.select{|article| article["published"] == true}
 end
-
 
 def display
 end
