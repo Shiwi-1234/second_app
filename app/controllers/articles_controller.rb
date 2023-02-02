fa9bd845781120640297abf31a232c67028e77be
Shiwi-1234 <shiwi@preciousinfosystem>
2023-02-02 14:14:35 +0530

    bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ def index
   url = "http://localhost:3001/articles"
   response = HTTParty.get(url)
   @data = JSON.parse(response.body)
-  
+ 
 end
 
 def show
@@ -15,13 +15,13 @@ end
 
 def edit
  url = "http://localhost:3001/articles/" + params[:id]
- @response = HTTParty.get(url)
- @data = JSON.parse(@response.body)
-end
+ response = HTTParty.get(url)
+ @data = JSON.parse(response.body)
+ end
 
 def update
   url = "http://localhost:3001/articles/"+ params[:id]
-  options = { headers: { "Accept" => "application/json", "Content-Type" => "application/json", }, body: {"title" => params['title'], "description" => params['description']}.to_json }
+  options = { headers: { "Accept" => "application/json", "Content-Type" => "application/json" }, body: {"title" => params['title'], "description" => params['description'] , "date" => params['date'], "day" => params["day"] , "published" => params["published"]}.to_json }
   response = HTTParty.patch(url, options)
   @data =  JSON.parse(response.body)
   if response.code == 200
@@ -35,10 +35,10 @@ def update
 end
 
 def published
-  url = "http://localhost:3001/articles"
+  url = "http://localhost:3001/articles/published_article"
   response = HTTParty.get(url)
   @data = JSON.parse(response.body)
-  @articles = @data.select{|article| article["published"] == true}
+  
 end
 
 def display
